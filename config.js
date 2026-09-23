b6a6fc04e72a7fab3a3155d4a0c0273923cf869e
demudewran720-oss <demudewran720@gmail.com>
2026-09-23 14:33:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = { // BU BOT SWENZY TARAFINDAN GELİŞTİRİLMİŞTİR ÇALMAYIN BULURUM SIZI
     bot: {
-        token: "Bot-Token"
+        token: "MTU1MTYzNTc0NzY3MTY0MjIyMw.GB7hP_.pOUcSlGHdIcH43XYMj3m1XhHdvRBlxj5ItJHNE"
     }
 }
